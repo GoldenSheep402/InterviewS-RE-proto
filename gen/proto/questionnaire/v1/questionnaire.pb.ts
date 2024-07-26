919674254466551aec6ebf742d67ce70e90780bd
Echin-h <Echin-h@users.noreply.github.com>
2024-07-26 12:55:04 +0000

    update: automatic generate new proto

---
+++ gen/proto/questionnaire/v1/questionnaire.pb.ts
@@ -0,0 +1,41 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+export type Questionnaire = {
+  id?: string
+  gid?: string
+  description?: string
+}
+
+export type QuestionsList = {
+  question?: Question
+  options?: Option[]
+}
+
+export type Question = {
+  id?: string
+  questionnaireId?: string
+  questionType?: string
+  questionText?: string
+}
+
+export type Option = {
+  id?: string
+  questionId?: string
+  optionText?: string
+}
+
+export type Response = {
+  id?: string
+  questionnaireId?: string
+  userId?: string
+}
+
+export type Answer = {
+  id?: string
+  responseId?: string
+  optionId?: string
+  answerText?: string
+}
---
+++ gen/proto/questionnaire/v1/questionnaire_service.pb.ts
@@ -0,0 +1,69 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+import * as UserV1User from "../../user/v1/user.pb"
+import * as QuestionnaireV1Questionnaire from "./questionnaire.pb"
+export type CreateQuestionnaireRequest = {
+  questionnaire?: QuestionnaireV1Questionnaire.Questionnaire
+  questions?: QuestionnaireV1Questionnaire.QuestionsList[]
+}
+
+export type CreateQuestionnaireResponse = {
+}
+
+export type UpdateQuestionnaireRequest = {
+  questionId?: string
+  options?: QuestionnaireV1Questionnaire.Option[]
+}
+
+export type UpdateQuestionnaireResponse = {
+}
+
+export type GetQuestionnaireRequest = {
+  questionnaireId?: string
+}
+
+export type GetQuestionnaireResponse = {
+  questionnaire?: QuestionnaireV1Questionnaire.Questionnaire
+  user?: UserV1User.User
+  questions?: QuestionnaireV1Questionnaire.QuestionsList[]
+}
+
+export type SubmitAnswerRequest = {
+  response?: QuestionnaireV1Questionnaire.Response
+  answers?: QuestionnaireV1Questionnaire.Answer[]
+}
+
+export type SubmitAnswerResponse = {
+}
+
+export type GetAnswerRequest = {
+  questionnaireId?: string
+}
+
+export type GetAnswerResponse = {
+  user?: UserV1User.User
+  answers?: QuestionnaireV1Questionnaire.Answer[]
+}
+
+export class QuestionnaireService {
+  static CreateQuestionnaire(req: CreateQuestionnaireRequest, initReq?: fm.InitReq): Promise<CreateQuestionnaireResponse> {
+    return fm.fetchReq<CreateQuestionnaireRequest, CreateQuestionnaireResponse>(`/gapi/questionnaire/v1/create`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static UpdateQuestionnaire(req: UpdateQuestionnaireRequest, initReq?: fm.InitReq): Promise<UpdateQuestionnaireResponse> {
+    return fm.fetchReq<UpdateQuestionnaireRequest, UpdateQuestionnaireResponse>(`/gapi/v1/questionnaire/update`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetQuestionnaire(req: GetQuestionnaireRequest, initReq?: fm.InitReq): Promise<GetQuestionnaireResponse> {
+    return fm.fetchReq<GetQuestionnaireRequest, GetQuestionnaireResponse>(`/gapi/v1/questionnaire/delete`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static SubmitAnswer(req: SubmitAnswerRequest, initReq?: fm.InitReq): Promise<SubmitAnswerResponse> {
+    return fm.fetchReq<SubmitAnswerRequest, SubmitAnswerResponse>(`/gapi/v1/questionnaire/submit`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetAnswer(req: GetAnswerRequest, initReq?: fm.InitReq): Promise<GetAnswerResponse> {
+    return fm.fetchReq<GetAnswerRequest, GetAnswerResponse>(`/gapi/v1/questionnaire/Info`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
